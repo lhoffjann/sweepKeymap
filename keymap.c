c9583e8279fe04e84db5dccb204840dab247bd2d
Lennart <lennart@Home.Maschine>
2023-07-04 08:26:09 +0200

    change minor layout

--- keymap.c
+++ keymap.c
@@ -28,29 +28,31 @@ enum ferris_tap_dances {
 #define KC_CLGV CTL_T(KC_GRV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( /* Colemak */
+  [0] = LAYOUT( /* Colemak */
 //
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,            KC_J,    KC_L,  KC_U,    KC_Y,   KC_SCLN,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,    KC_L             ,  KC_U             ,    KC_Y,   KC_BSPC,
 //
-    KC_CTLA, KC_R,    KC_S,    KC_T,    KC_G,            KC_M,    KC_N,  KC_E,    KC_I,   KC_O,
+    KC_A,   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,             KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    KC_I,   KC_O,
 //
-    KC_Z, KC_X,    KC_C,    KC_D,       KC_V,            KC_K,    KC_H,  KC_COMM, KC_DOT, KC_RLSH,
+    KC_Z,   KC_X,    KC_C             ,    KC_D             , KC_V,             KC_K,    KC_H,  KC_COMM, KC_DOT, KC_RLSH,
 //
-                                    KC_CLGV, KC_BSM1, KC_SPM2, KC_GUTA
+                                            KC_SPACE, OSM(MOD_LSFT),            OSM(MOD_RCTL), TO(1)
   ),
 
-  [_LOWER] = LAYOUT( /* [> LOWER <] */
+[1] = LAYOUT( /* Colemak */
 //
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,    KC_L             ,  KC_U             ,    KC_Y,   KC_SCLN,
 //
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_A,   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,             KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    KC_I,   KC_O,
 //
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
+    KC_Z,   KC_X,    KC_C             ,    KC_D             , KC_V,             KC_K,    KC_H,  KC_COMM, KC_DOT, KC_RLSH,
 //
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                                            TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(1)
   ),
 
-  [_RAISE] = LAYOUT( /* [> RAISE <] */
+
+  [2] = LAYOUT( /* [> RAISE <] */
+
 //
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
 //
