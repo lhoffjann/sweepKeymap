1b3bc4a2f4e634c94b883b2a71ae6bfce0d33591
Lennart <hoff.lenn@gmail.com>
2023-11-20 09:08:58 +0100

    added display layout

--- keymap.c
+++ keymap.c
@@ -27,16 +27,18 @@ enum ferris_tap_dances {
 #define KC_AE RALT(KC_Q)
 #define KC_SZ RALT(KC_S)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
   [0] = LAYOUT( /* Colemak */
 //
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,    KC_L             ,  KC_U             ,    KC_Y,   KC_BSPC,
 //
-    KC_A,   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,             KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    KC_I,   KC_O,
+    KC_A,   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,             KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    LT(9,KC_I),   KC_O,
 //
    LT(5, KC_Z),   KC_X,    KC_C             ,    KC_D             , KC_V,             KC_K,    KC_H,  KC_COMM, KC_DOT, LT(5, KC_RLSH),
 //
                                             KC_SPACE, OSM(MOD_LSFT),            OSM(MOD_RCTL), TO(1)
   ),
+
 [1] = LAYOUT( /* Colemak */
 //
     KC_ESCAPE,    KC_AT,   KC_PIPE,KC_EXLM, KC_LPRN,                                    KC_RPRN, KC_AMPR,  KC_GRV,    KC_BSLS,   KC_BSPC,
@@ -60,6 +62,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(2)
   ),
 
+
+[9] = LAYOUT( /* Colemak */
+//
+    KC_ESCAPE,    G(KC_7),  G(KC_8), G(KC_9), G(KC_0),                                     KC_F9, KC_F10,  KC_F11,    KC_F12,   KC_BSPC,
+//
+    KC_TAB,   G(KC_4),   G(KC_5), G(KC_6), KC_NO,             KC_F5, MT(MOD_RGUI,KC_F6), MT(MOD_RALT, KC_F7),KC_F8,   KC_ENTER,
+//
+    KC_NO,   G(KC_1),   G(KC_2), G(KC_3), KC_NO,                                        KC_F1,    KC_F2,  KC_F3, KC_F4, TO(3),
+//
+            TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(2)
+  ),
 
   [3] = LAYOUT( /* [> RAISE <] */
 
