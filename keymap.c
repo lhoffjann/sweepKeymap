09dec4fb21e6711688509134a0f0f1e0a5ddae55
lennart hoffjann <lennart.hoffjann@klosebrothers.de>
2023-11-24 09:30:25 +0100

    changing layout so some layers are more accessible

--- keymap.c
+++ keymap.c
@@ -9,7 +9,14 @@
 // entirely and just use numbers.
 
 
-
+const uint16_t PROGMEM boot_combo[] = {KC_Q, KC_B, KC_J, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM number_combo[] = {KC_G, KC_M, COMBO_END};
+const uint16_t PROGMEM navi_combo[] = { MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E), COMBO_END};
+combo_t key_combos[] = {
+    COMBO(boot_combo, QK_BOOT),
+    COMBO(number_combo, TO(2)),
+    COMBO(navi_combo, TO(6)),
+};
 enum ferris_tap_dances {
   TD_Q_ESC
 };
@@ -28,86 +35,74 @@ enum ferris_tap_dances {
 #define KC_SZ RALT(KC_S)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT( /* Colemak */
+[0] = LAYOUT( /* Base layer*/
 //
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,    KC_L             ,  KC_U             ,    KC_Y,   KC_BSPC,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                       KC_J,    KC_L             ,  KC_U             ,    KC_Y,   KC_BSPC,
 //
-    KC_A,   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,             KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    LT(9,KC_I),   KC_O,
+    LT(6,KC_A),   KC_R,   MT(MOD_LALT, KC_S),    MT(MOD_LGUI,KC_T), KC_G,                 KC_M,    MT(MOD_RGUI,KC_N),  MT(MOD_RALT,KC_E),    LT(9,KC_I),   KC_O,
 //
-   LT(5, KC_Z),   KC_X,    KC_C             ,    KC_D             , KC_V,             KC_K,    KC_H,  KC_COMM, KC_DOT, LT(5, KC_RLSH),
+    LT(5, KC_Z),   KC_X,    KC_C,    KC_D, KC_V,                                    KC_K,    KC_H,  KC_COMM, KC_DOT, LT(5, KC_RLSH),
 //
-                                            KC_SPACE, OSM(MOD_LSFT),            OSM(MOD_RCTL), TO(1)
+                                                    KC_SPACE, OSM(MOD_LSFT), OSM(MOD_RCTL), TO(1)
   ),
 
-[1] = LAYOUT( /* Colemak */
+[1] = LAYOUT( /* Symbol layer*/
 //
-    KC_ESCAPE,    KC_AT,   KC_PIPE,KC_EXLM, KC_LPRN,                                    KC_RPRN, KC_AMPR,  KC_GRV,    KC_BSLS,   KC_BSPC,
+    KC_ESCAPE,    KC_AT,   KC_PIPE,KC_EXLM, KC_LPRN,                                KC_RPRN, KC_AMPR,  KC_GRV,    KC_BSLS,   KC_BSPC,
 //
-    KC_TAB,   KC_DLR,  MT(MOD_LALT, KC_SCLN),MT(MOD_LGUI,KC_QUOTE), KC_LCBR,             KC_RCBR,MT(MOD_RGUI, KC_PLUS),  MT(MOD_RALT,KC_MINS),KC_ASTR,   KC_ENTER,
+    KC_TAB, KC_DLR,  MT(MOD_LALT, KC_SCLN),MT(MOD_LGUI,KC_QUOTE), KC_LCBR,          KC_RCBR,MT(MOD_RGUI, KC_PLUS),  MT(MOD_RALT,KC_MINS),KC_ASTR,   KC_ENTER,
 //
-    KC_PLUS,   KC_CIRC,   KC_PERC, KC_DQUO, KC_LBRC,                                   KC_RBRC,    KC_HASH, S(KC_SCLN), KC_UNDS, KC_TILD,
+    KC_PLUS,   KC_CIRC,   KC_PERC, KC_DQUO, KC_LBRC,                                KC_RBRC,    KC_HASH, S(KC_SCLN), KC_UNDS, KC_TILD,
 //
-            TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(2)
-  ),
-
-
-[2] = LAYOUT( /* Colemak */
-//
-    KC_ESCAPE,    KC_7,  KC_8, KC_9, KC_0,                                     KC_F9, KC_F10,  KC_F11,    KC_F12,   KC_BSPC,
-//
-    KC_TAB,   KC_4,  MT(MOD_LALT, KC_5), MT(MOD_LGUI,KC_6), KC_NO,             KC_F5, MT(MOD_RGUI,KC_F6), MT(MOD_RALT, KC_F7),KC_F8,   KC_ENTER,
-//
-    KC_NO,   KC_1,   KC_2, KC_3, KC_NO,                                        KC_F1,    KC_F2,  KC_F3, KC_F4, TO(3),
-//
-            TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(2)
+                                                    TO(0),OSM(MOD_LSFT),OSM(MOD_RCTL), TO(1)
   ),
 
 
-[9] = LAYOUT( /* Colemak */
+[2] = LAYOUT( /* Number layer*/
 //
-    KC_ESCAPE,    G(KC_7),  G(KC_8), G(KC_9), G(KC_0),                                     KC_F9, KC_F10,  KC_F11,    KC_F12,   KC_BSPC,
+    KC_NO,    KC_7,  KC_8, KC_9, KC_0,                                              KC_F9, KC_F10,  KC_F11,    KC_F12,   KC_BSPC,
 //
-    KC_TAB,   G(KC_4),   G(KC_5), G(KC_6), KC_NO,             KC_F5, MT(MOD_RGUI,KC_F6), MT(MOD_RALT, KC_F7),KC_F8,   KC_ENTER,
+    KC_NO,   KC_4,   KC_5, KC_6, KC_NO,                                             KC_F5, MT(MOD_RGUI,KC_F6), MT(MOD_RALT, KC_F7),KC_F8,   KC_ENTER,
 //
-    KC_NO,   G(KC_1),   G(KC_2), G(KC_3), KC_NO,                                        KC_F1,    KC_F2,  KC_F3, KC_F4, TO(3),
+    KC_NO,   KC_1,   KC_2, KC_3, KC_NO,                                             KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,
 //
-            TO(0),   OSM(MOD_LSFT),             OSM(MOD_RCTL), TO(2)
+                                                    TO(0), OSM(MOD_LSFT), OSM(MOD_RCTL), TO(1)
   ),
 
-  [3] = LAYOUT( /* [> RAISE <] */
+
+[9] = LAYOUT( /* Display layer*/
+//
+    KC_ESCAPE,    G(KC_7),  G(KC_8), G(KC_9), G(KC_0),                              KC_F9, KC_F10,  KC_F11,    KC_F12,   KC_BSPC,
+//
+    KC_TAB,   G(KC_4),   G(KC_5), G(KC_6), KC_NO,                                   KC_F5, MT(MOD_RGUI,KC_F6), MT(MOD_RALT, KC_F7),KC_F8,   KC_ENTER,
+//
+    KC_NO,   G(KC_1),   G(KC_2), G(KC_3), KC_NO,                                    KC_F1,    KC_F2,  KC_F3, KC_F4, KC_NO,
+//
+                                                    TO(0), OSM(MOD_LSFT), OSM(MOD_RCTL), TO(2)
+  ),
+ [6] = LAYOUT( /* Arrow layer */
 
 //
-    TO(4),   KC_MS_BTN1,   KC_MS_UP,   KC_MS_BTN2,   KC_WH_U,           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                      KC_DOWN,   KC_RIGHT,   KC_NO,   KC_NO,   KC_NO,
 
-    KC_NO,  KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_WH_D,                  KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_NO,
+    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,                                             KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,
 //
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                              KC_UP, KC_LEFT, KC_NO, KC_NO, KC_NO,
 //
-                                    TO(0), KC_TRNS, KC_TRNS, TO(1)
+                                                        TO(0), KC_TRNS, KC_TRNS, TO(1)
 
   ),
-    [4] = LAYOUT( /* [> RAISE <] */
+    [5] = LAYOUT( /* Umlaut layer  */
 
 //
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT,
+    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,                            KC_TRNS,   KC_TRNS,   KC_UE,   KC_TRNS,   KC_TRNS,
 
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,
+    KC_AE,   KC_TRNS,     KC_SZ,  KC_TRNS,   KC_TRNS,                               KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_OE,
 //
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-//
-                                    TO(0), KC_TRNS, KC_TRNS, TO(1)
-
-  ),
-    [5] = LAYOUT( /* [> RAISE <] */
+   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS,   KC_TRNS,                            KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
 
 //
-    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,           KC_TRNS,   KC_TRNS,   KC_UE,   KC_TRNS,   KC_TRNS,
-    KC_AE,   KC_TRNS,     KC_SZ,  KC_TRNS,   KC_TRNS,          KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_OE,
-//
-   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS,   KC_TRNS,          KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-
-//
-                                    KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS
+                                                        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS
   )
 };
 
